edc3d9d8e8ba6f6823f1d43520a638d04fbe34ec
lmandres <lmandres@users.noreply.github.com>
2020-10-16 18:18:47 -0400

    Update setup_ofono.sh to set ofono online.

--- setup_ofono.sh
+++ setup_ofono.sh
@@ -20,6 +20,17 @@ while [ 1 ]; do
     if [ -z "$dbstat" ]; then
         break
     fi
-    echo "Waiting for dbus . . ."
+    echo "Waiting for dbus to power phonesim . . ."
+    sleep 1
+done
+
+while [ 1 ]; do
+    dbout=$(DISPLAY=:1 dbus-send --print-reply --system --dest=org.ofono /phonesim org.ofono.Modem.SetProperty string:"Online" variant:boolean:"true" 2>&1)
+    #echo $dbout
+    dbstat=$(echo $dbout | grep -a "Operation failed")
+    if [ -z "$dbstat" ]; then
+        break
+    fi
+    echo "Waiting for dbus to put phonesim online . . ."
     sleep 1
 done
